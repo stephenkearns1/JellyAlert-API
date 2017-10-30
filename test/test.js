469114ab07647b5f4119665b13443154777538e7
JohnDoe <x@JohnDoes-MacBook-Pro.local>
2017-10-30 15:32:23 +0000

    Added tests

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('Test', function() {
+    it('should pass', function() {
+        var result = 4;
+        assert.equal(result,4);
+    });
+
+
+});
